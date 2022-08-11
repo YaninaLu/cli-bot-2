9ae3398e63d4cc149e88f9e8673575145eec45b3
Yanina Lubenska <yaninaosadchaya@gmail.com>
2022-08-11 11:18:01 +0300

    Added info.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# cli-bot-2
+# CLI Assistant Bot
+
+This is an assistant bot that helps you to manage your address book.
+
+It is a command line application, which can greet you, add new contacts to your address book, change existing ones, 
+show phones of the given people or the whole phonebook. There are no restrictions on the name format, while there are 
+restrictions on the phone format, i.e. it has to be given either in format +123456789012 or 1234567890.
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from re import match
 
 class CliApp:
     """
-    A class that is responsible for communicating with the user.
+    Communicates with the user.
 
-    It takes input from the user, parses it and sends it to the AssistantBot class. Also, it outputs the result that
+    Takes input from the user, parses it and sends it to the assistant bot. It outputs the result that
     returns from the bot and terminates the app when the user inputs one of the stop words.
     """
 
@@ -46,12 +46,9 @@ class CliApp:
 
 class AssistantBot:
     """
-    A class that is responsible for identifying and calling functions that correspond to the given command.
-
-    It stores commands and corresponding functions in a dictionary and calls these functions with the given arguments.
-    It also catches exceptions on the domain level. When the user calls commands connected with managing the adressbook,
-    bot sends them to the PhoneBook class.
+    Identifies and calls functions that correspond to the users commands. Catches exceptions on the domain level.
     """
+
     def __init__(self):
         self.commands = {
             "hello": self.hello,
@@ -59,8 +56,7 @@ class AssistantBot:
             "delete": self.delete,
             "change": self.change,
             "phone": self.show,
-            "show": self.show,
-            "save": self.save_name
+            "show": self.show
         }
         self.addressbook = AddressBook()
 
@@ -91,8 +87,7 @@ class AssistantBot:
     @input_error
     def handle(self, command: str, args: List[str]) -> str:
         """
-        An entry point for the commands given by the user. It takes a corresponding function from the dictionary and
-        calls its instance with the given arguments.
+        An entry point for the commands given by the user. It takes a function and calls it with the given arguments.
 
         :param command: command given by the user
         :param args: arguments to call the function with
@@ -112,7 +107,7 @@ class AssistantBot:
         return "How can I help you?"
 
     @input_error
-    def add(self, name: str, phone=None) -> None:
+    def add(self, name: str, phone: None | str = None) -> None:
         """
         Calls a function that adds the given phone number to the addressbook.
 
@@ -121,32 +116,25 @@ class AssistantBot:
         """
         if name in self.addressbook.data:
             record = self.addressbook.data[name]
-            phone_object = Phone()
-            phone_object.save_phone(phone)
-            return record.add_phone(phone_object)
+            return record.add_phone(phone)
         else:
-            record = Record()
-            record.save_name(name)
+            record = Record(name)
             if phone:
-                phone_object = Phone()
-                phone_object.save_phone(phone)
-                record.add_phone(phone_object)
+                record.add_phone(phone)
             return self.addressbook.add_record(record)
 
-
     @input_error
-    def change(self, name: str, new_phone: str) -> None:
+    def change(self, name: str, old_phone: str, new_phone: str) -> None:
         """
         Calls a function that changes the phone number of the given person.
 
         :param new_phone: a new phone number
-        :param phone_to_change: an old phone number that user wants to change
+        :param old_phone: a number to delete
         :param name: name that has to be already present in the addressbook
         """
         record = self.addressbook.data[name]
-        phone_object = Phone()
-        phone_object.save_phone(new_phone)
-        return record.change_phone(phone_object)
+        record.delete_phone(old_phone)
+        return record.add_phone(new_phone)
 
     @input_error
     def show(self, name: str) -> str:
@@ -158,36 +146,134 @@ class AssistantBot:
         """
         return self.addressbook.show_phone(name)
 
-    def delete(self, name, phone):
+    def delete(self, name: str, phone: str) -> None:
+        """
+        Calls a function that deletes the given phone number from the record of the given person.
+
+        :param name: whose phone the user wants to delete
+        :param phone: a phone to delete
+        """
         record = self.addressbook.data[name]
         return record.delete_phone(phone)
 
-    def save_name(self, name):
-        record = Record()
-        record.save_name(name)
-        return self.addressbook.add_record(record)
+
+class Field:
+    """
+    The base class for the fields of the Record class.
+    """
+
+    def __init__(self):
+        self.value = None
+
+    def __str__(self):
+        return f"{self.value}"
+
+    def __hash__(self) -> int:
+        return self.value.__hash__()
+
+    def __eq__(self, o: object) -> bool:
+        return self.value == o
+
+
+class Name(Field):
+    """
+    A name of a person in an address book.
+    """
+
+    def __init__(self, name: str):
+        super().__init__()
+        self.value = name
+
+
+class Phone(Field):
+    """
+    A phone number of a person in an address book.
+    """
+
+    def __init__(self, phone: str):
+        super().__init__()
+        self._verify(phone)
+        self.value = phone
+
+    @staticmethod
+    def _verify(phone: str):
+        """
+        Checks if the phone number is valid. Throws exception if it's not valid.
+
+        :param phone: phone number as a string
+        :return: exception if it doesn't match the needed pattern, otherwise True
+        """
+        if not match(r"(\+?\d{12}|\d{10})", phone):
+            raise ValueError("Invalid phone format. Try +123456789012 or 1234567890.")
+
+
+class Record:
+    """
+    Users name and phones. Name field is compulsory, while phones field is optional and can be omitted.
+    """
+
+    def __init__(self, name: str):
+        if not Name:
+            raise ValueError("The record can't be without a name")
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone: str) -> None:
+        """
+        Adds a phone to the record.
+
+        :param phone: phone number
+        """
+        phone_object = Phone(phone)
+        self.phones.append(phone_object)
+
+    def delete_phone(self, phone: str) -> None:
+        """
+        Deletes a phone from the record.
+
+        :param phone: a phone number to delete
+        """
+        for phone_object in self.phones:
+            if phone_object.phone == phone:
+                self.phones.remove(phone_object)
+                return
+        raise ValueError("The given phone is not in a list.")
+
+    def __str__(self):
+        phones = ", ".join(map(lambda phone: str(phone), self.phones))
+        return f"Name: {self.name}, phones: {phones}\n"
 
 
 class AddressBook(UserDict):
-    def add_record(self, record):
-        if record.name.value not in self.data:
-            self.data[record.name.value] = record
+    """
+    An address book.
+    """
+
+    def add_record(self, record: Record) -> None:
+        """
+        Adds a new record to the address book.
+
+        :param record: a record to add
+        """
+        if record.name not in self.data:
+            self.data[record.name] = record
         else:
             raise ValueError(
-                "This name is already in your phonebook. If you want to change the phone number, type 'change'.")
+                "This name is already in your phonebook. If you want to change the phone number, type 'change'."
+                "If you want to add a phone number, type it after the name.")
 
-    def show_phone(self, name):
+    def show_phone(self, name: str) -> str:
         """
         Shows the phone number of a given person. If 'all' was given as an argument it returns the whole phonebook.
 
         :param name: name to show or 'all'
-        :return: phone number or phonebook as a string
+        :return: phone number or addressbook as a string
         """
         if name == "all":
             if self.data:
                 return str(self)
             else:
-                return "You do not have any contacts yet."
+                "You do not have any contacts yet."
         else:
             return str(self.data[name])
 
@@ -196,70 +282,6 @@ class AddressBook(UserDict):
         for name, record in self.data.items():
             result += str(record)
         return result
-
-
-class Record:
-    def __init__(self):
-        self.name = None
-        self.phones = []
-
-    def save_name(self, name):
-        self.name = Name(name)
-
-    def add_phone(self, phone):
-        self.phones.append(phone)
-
-    def delete_phone(self, phone):
-        for phone_object in self.phones:
-            if phone_object.phone == phone:
-                self.phones.remove(phone_object)
-
-    def change_phone(self, new_phone):
-        self.phones.clear()
-        self.phones.append(new_phone)
-
-    def __str__(self):
-        phones = ", ".join(map(lambda phone: str(phone), self.phones))
-        return f"Name: {self.name}, phones: {phones}\n"
-
-
-class Field:
-    def __init__(self):
-        self.value = None
-
-
-class Name(Field):
-    def __init__(self, name):
-        super().__init__()
-        self.value = name
-
-    def __str__(self):
-        return f"{self.value}"
-
-class Phone(Field):
-    def __init__(self):
-        super().__init__()
-        self.phone = None
-
-    def verify_phone(self, phone):
-        """
-        Checks if the phone number is valid. Throws exception if it's not valid.
-
-        :param phone: phone number as a string
-        :return: exception if it doesn't match the needed pattern
-        """
-        if not match(r"(\+?\d{12}|\d{10})", phone):
-            raise ValueError("Invalid phone format")
-        return True
-
-    def save_phone(self, phone):
-        if self.verify_phone(phone):
-            self.phone = phone
-        else:
-            raise ValueError("Invalid phone format. Try +123456789012 or 1234567890.")
-
-    def __str__(self):
-        return f"{self.phone}"
 
 
 def main():
